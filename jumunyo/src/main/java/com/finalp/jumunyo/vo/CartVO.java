d166f96bbf993933e307979cad87ff3a4a57dc91
dndkr6723 <57051145+dndkr6723@users.noreply.github.com>
2020-04-22 15:21:08 +0900

    Add files via upload

---
+++ jumunyo/src/main/java/com/finalp/jumunyo/vo/CartVO.java
@@ -0,0 +1,10 @@
+package com.finalp.jumunyo.vo;
+
+public class CartVO {
+	
+	private String cartmenu_id;
+	private String cartmenu_name;
+	private int cartmenu_price;
+	private int cartmenu_count;
+	
+}
---
+++ jumunyo/src/main/java/com/finalp/jumunyo/vo/CategoryVO.java
@@ -0,0 +1,5 @@
+package com.finalp.jumunyo.vo;
+
+public class CategoryVO {
+
+}
